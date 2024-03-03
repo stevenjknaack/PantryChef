7464963384eb44856deeda2fc69d3968a9f935dd
SJKNAACK <sjknaack@wisc.edu>
2024-03-03 16:16:14 -0600

    removed extraneous import

--- backend/src/main/java/com/pantrychef/backend/reviews/Review.java
+++ backend/src/main/java/com/pantrychef/backend/reviews/Review.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Reference;
 
 import java.util.List;
 
