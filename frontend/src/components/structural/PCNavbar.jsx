0c8ebc1de919fcb19fa9cf61dae6830d436272d7
SJKNAACK <sjknaack@wisc.edu>
2024-03-17 14:57:41 -0500

    fixed selectOnCollapse behavior

--- frontend/src/components/structural/PCNavbar.jsx
+++ frontend/src/components/structural/PCNavbar.jsx
@@ -1,15 +1,21 @@
+import { useState } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 export default function PCNavbar() {
-    return <Navbar bg='dark' data-bs-theme='dark' fixed='top' expand='sm' collapseOnSelect>
+    const [expanded, setExpanded] = useState();
+
+    const collapse = () => setExpanded(false);
+    const toggleExpanded = () => setExpanded(curr => !curr);
+
+    return <Navbar bg='dark' data-bs-theme='dark' fixed='top' expand='sm' expanded={expanded}>
         <Container>
             <Navbar.Brand as={Link} to='/'>PantryChef</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={toggleExpanded}/>
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav>
-                    <Nav.Link as={Link} to='/login'>Login</Nav.Link>
-                    <Nav.Link as={Link} to='/register'>Register</Nav.Link>
+                    <Nav.Link as={Link} to='/login' onClick={collapse}>Login</Nav.Link>
+                    <Nav.Link as={Link} to='/register' onClick={collapse}>Register</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Container>
