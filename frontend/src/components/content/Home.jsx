d06e86c1433b8e8d7ce38f44714b39a11a886e3c
SJKNAACK <sjknaack@wisc.edu>
2024-04-13 16:02:37 -0500

    removed connection tests on the home screen

--- frontend/src/components/content/Home.jsx
+++ frontend/src/components/content/Home.jsx
@@ -1,21 +1,11 @@
 import { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
-//
-export default function Home(props) {
-    const alertIngredient = () => {
-        fetch('http://localhost:8080/recipes', {
-            credentials: 'include',
-            method: 'GET'
-        })
-            .then((response) => response.json())
-            .then((data) => console.log(data));
-    };
 
+export default function Home(props) {
     return (
         <>
             <h1>Welcome to PantryChef!</h1>
             <p>More To Come :)</p>
-            <Button onClick={alertIngredient}>Press</Button>
         </>
     );
 }
