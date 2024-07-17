6106ead5bbda55263fe6552ba63abda929509fa7
SJKNAACK <sjknaack@wisc.edu>
2024-07-17 17:33:24 -0500

    simplified custom error handling

--- backend/src/main/java/com/pantrychef/backend/errors/RequestExceptionHandler.java
+++ backend/src/main/java/com/pantrychef/backend/errors/RequestExceptionHandler.java
@@ -23,54 +23,6 @@ import java.util.Map;
 @ControllerAdvice
 public class RequestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler()
-    public ResponseEntity<Object> handleAccessDeniedException(
-            AccessDeniedException ex, WebRequest request
-    ) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("msg", "Access denied");
-        body.put("status", HttpStatus.FORBIDDEN);
-
-        return handleExceptionInternal(ex, body,
-                new HttpHeaders(), HttpStatus.FORBIDDEN, request);
-    }
-
-    @ExceptionHandler()
-    public ResponseEntity<Object> handleAuthenticationException(
-            AuthenticationException ex, WebRequest request
-    ) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("msg", "Authentication Failed");
-        body.put("status", HttpStatus.UNAUTHORIZED);
-
-        return handleExceptionInternal(ex, body,
-                new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
-    }
-
-    @ExceptionHandler()
-    public ResponseEntity<Object> handleDataAccessException(
-            DataAccessException ex, WebRequest request
-    ) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("msg", "There was an error with your request");
-        body.put("status", HttpStatus.BAD_REQUEST);
-
-        return handleExceptionInternal(ex, body,
-                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
-    }
-
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResponseEntity<Object> handleMethodArgumentNotValidException(
-//            MethodArgumentNotValidException ex, WebRequest request
-//    ) {
-//        Map<String, Object> body = new HashMap<>();
-//        body.put("msg", "There was an error with validating your request");
-//        body.put("status", HttpStatus.BAD_REQUEST);
-//
-//        return handleExceptionInternal(ex, body,
-//                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
-//    }
-
-    @ExceptionHandler()
     public ResponseEntity<Object> handleCustomExceptions(
             ResponseStatusException ex, WebRequest request
     ) {
@@ -80,17 +32,5 @@ public class RequestExceptionHandler extends ResponseEntityExceptionHandler {
 
         return handleExceptionInternal(ex, body,
                 new HttpHeaders(), ex.getStatusCode(), request);
-    }
-
-    @ExceptionHandler()
-    public ResponseEntity<Object> handleAllExceptions(
-            Exception ex, WebRequest request
-    ) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("msg", "Something went wrong with the server");
-        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
-
-        return handleExceptionInternal(ex, body,
-                new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 }
